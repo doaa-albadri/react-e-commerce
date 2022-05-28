d334936733917739df9a5dd97e9daed06a649990
Doaa Albadri <doaabadri13@gmail.com>
2022-05-28 04:53:05 -0700

    add routing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
 import "./App.css";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  Routes,
+} from "react-router-dom";
+import Navbar from "./layouts/navbar";
+import Header from "./layouts/header";
+import Banner from "./components/banner";
+import TopCards from "./components/topCards";
+import Footer from "./layouts/footer";
 import Home from "./pages/Home";
+import Product from "./pages/Product";
 
 function App() {
   return (
     <div className="App">
-      <Home />
+      <Router>
+        <Header />
+        <Navbar />
+        <Banner />
+        <TopCards />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/product" element={<Product />} />
+        </Routes>
+        {/* <Footer /> */}
+      </Router>
     </div>
   );
 }
