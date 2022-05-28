1a7f1816b8c519c371cdcaf72ff094f5d6229a27
Doaa Albadri <doaabadri13@gmail.com>
2022-05-28 04:54:45 -0700

    removed app components

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,20 +1,10 @@
 import React from "react";
-import Footer from "../../layouts/footer";
-import Header from "../../layouts/header";
-import Navbar from "../../layouts/navbar";
-import Banner from "../../components/banner";
-import TopCards from "../../components/topCards";
 import Products from "./products";
 
 const Home = () => {
   return (
     <>
-      <Header />
-      <Navbar />
-      <Banner />
-      <TopCards />
       <Products />
-      {/* <Footer /> */}
     </>
   );
 };
