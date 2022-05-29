aeace02cb805c819c9143a729f659e36ab839703
Doaa Albadri <doaabadri13@gmail.com>
2022-05-29 15:05:13 -0700

    add pages links

--- src/components/topCards/index.jsx
+++ src/components/topCards/index.jsx
@@ -3,12 +3,13 @@ import "./topCards.css";
 import clothes from "../../assets/images/clothes.jpg";
 import makeup from "../../assets/images/makeup.jpg";
 import accessories from "../../assets/images/accessories.jpg";
+import { Link } from "react-router-dom";
 
 const TopCards = () => {
   const cards = [
-    { image: clothes, title: "Clothes" },
-    { image: makeup, title: "Make-Up" },
-    { image: accessories, title: "Accessories" },
+    { image: clothes, title: "Clothes", path: "clothes" },
+    { image: makeup, title: "Make-Up", path: "makeup" },
+    { image: accessories, title: "Accessories", path: "accessories" },
   ];
 
   return (
@@ -18,7 +19,9 @@ const TopCards = () => {
           <div key={card.title} className="img-container">
             <img src={card.image} />
             <div className="titles-div">
-              <h4 className="title">{card.title}</h4>
+              <Link className="link" to={card.path}>
+                <h4 className="title">{card.title}</h4>
+              </Link>
             </div>
           </div>
         );
